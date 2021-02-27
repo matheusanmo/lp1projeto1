285e3ab2779130a3c8ec82298f4aa541f7cd3859
matheus anmo <matheusanmo@gmail.com>
2021-02-27 16:22:43 -0300

    versao quebrada de gen_gametables

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,31 @@ using std::endl;
 #include <string>
 using std::string;
 using std::stoi;
+using std::getline;
 
 #include <fstream>
 using std::ifstream;
+
+#include <sstream>
+using std::stringstream;
 
 const int    DEFAULT_NCHECK_NUM        = 3;
 const string DEFAULT_INPUT_PUZZLE_FILE ("input.txt");
 
 struct Config {
-    int error; // codigo de erro caso programa seja mal invocado. 0 = tudo ok
-    bool help;
-    int ncheck;
+    int    error; // codigo de erro caso programa seja mal invocado. 0 = tudo ok
+    bool   help;
+    int    ncheck;
     string input_puzzle_file;
+};
+
+struct GameTable {
+    int table[9][9];
+};
+
+struct GameTables {
+    int  tablecount;
+    GameTable* tables;
 };
 
 /**
@@ -33,6 +46,72 @@ bool file_exists(string filepath) {
 }
 
 /**
+ * Destroi um `GameTables` deletando seu membro que eh ponteiro para lista de 
+ * `GameTable`s.
+ * 
+ * @param   gametables  NAO recebe ponteiro mas sim a variavel em si
+ */
+void destroy_gametables(GameTables gametables) {
+    delete[] gametables.tables;
+    return;
+}
+
+/**
+ * Recebe string com caminho de um arquivo a parseado como GameTables. Descarta
+ * tables que nao consegue parsear e avisa, mas nao gera erros ou para a execucao do 
+ * programa. Nao checa se o arquivo existe ou eh acessivel; isto deve ser feito
+ * de antemao. Espera-se que os tabuleiros estejam no seguinte formato:
+ * 9 inteiros, com ou sem sinal, separados por whitespace (fora \n) seguidos
+ * por uma linha vazia (mesmo sendo o ultimo tabuleiro do arquivo). O arquivo
+ * deve comecar imediatamente com a primeira fileira do primeiro tabuleiro, isto
+ * eh, nada de whitespace ou comentarios antes.  Se o membro `gametables` do retorno 
+ * eh nullptr, um erro ocorreu.
+ * 
+ * @param   input_puzzle_file   caminho do arquivo
+ * @returns GameTables com tables que foram parseadas com sucesso
+ */
+GameTables gen_gametables(string input_puzzle_file) {
+    GameTables gametables { 0, nullptr };
+    int tablecount = 1;
+    int linecount  = 1;
+    ifstream input_stream (input_puzzle_file);
+    string line;
+    while (getline(input_stream, line)) {
+        linecount++;
+    }
+    // caso tenhamos recebido input mal formatado, esta eh uma forma de descobrir
+    // que tambem nos diz a quantidade de tabuleiros declarados.
+    if (linecount % 10 != 0) {
+        cout << "Erro: input_puzzle_file contem algum tabuleiro mal formatado (linhas a mais ou a menos). `linecount` = " << linecount << endl;
+        return gametables;
+    }
+    tablecount            = linecount / 10; // 10 linhas por tabuleiro (9 de numero + linha vazia)
+    // tables eh ponteiro para array de `tablecount` `GameTable`s vazios (?) 
+    GameTable* tables     { new GameTable[tablecount]{} }; 
+    gametables.tables     = tables; // LEMBRAR de deletar usando `delete[] tables`
+    gametables.tablecount = tablecount;
+
+    // resetando posicao no arquivo (nao consegui com `seekg` por alguma razao)
+    input_stream.close();
+    input_stream = ifstream(input_puzzle_file);
+    int table_index{}, line_index{}, row_index{};
+    for (table_index = 0; table_index < tablecount; table_index++) {
+        for (line_index = 0; line_index < 9; line_index++) { 
+            for (row_index = 0; row_index < 9; row_index++) {
+                int readnumber = 0;
+                input_stream >> readnumber;
+                cout << readnumber << " ";
+                tables[tablecount].table[line_index][row_index] = readnumber;
+            }
+            int throwaway{};
+            //input_stream >> throwaway;
+        }
+    }
+    //input_stream.close();
+    return gametables;
+}
+
+/**
  * recebe argc e argv como os do main e retorna Config equivalente
  *
  * @param   argc    argc como passado para main()
@@ -41,12 +120,12 @@ bool file_exists(string filepath) {
  */
 Config make_config(int argc, char** argv) {
     // esta eh a configuracao padrao
-    Config conf = { 0, false, DEFAULT_NCHECK_NUM, DEFAULT_INPUT_PUZZLE_FILE };
+    Config conf { 0, false, DEFAULT_NCHECK_NUM, DEFAULT_INPUT_PUZZLE_FILE };
     if (argc == 1) {
         // invocado sem argumentos
         return conf;
     }
-    for (int i=1; i<argc; i++) { // i=1 para pular nome do programa
+    for (int i = 1; i < argc; i++) { // i=1 para pular nome do programa
         string arg (argv[i]);
         if (arg == "--help") {
             conf.help = true;
@@ -72,7 +151,7 @@ Config make_config(int argc, char** argv) {
             return conf;
         }
     }
-    return conf; // evitando warning do compilador
+    return conf; // fluxo nao deve chegar aqui mas evita warning do compilador
 }
 
 /**
@@ -87,30 +166,53 @@ void print_help() {
     return;
 }
 
+/**
+ * printa na tela a gametable indentada com `offset` espacos antes do primeiro
+ * numero (padrao 0).
+ *
+ * @params  gametable   tabela que sera printada
+ * @params  offset      indentacao da table (padrao 0)
+ */
+void print_gametable(GameTable gametable, int offset = 0) {
+    string padding (offset, ' ');
+    for (int line_index = 0; line_index < 9; line_index++) {
+        cout << padding;
+        for (int row_index = 0; row_index < 9; row_index++) {
+            cout << gametable.table[line_index][row_index] << " ";
+        }
+        cout << endl;
+    }
+    return;
+}
+
 int main(int argc, char **argv) 
 {
     Config config = make_config(argc, argv);
-    // lidando com erros avisados pela config
     if (config.error != 0) {
         if (config.error == -1) {
             cout << "Erro: `--ncheck` espera argumento." << endl;
             return -1;
-        } else {
-            cout << "Erro: `config.erro` acusa erro " << config.error << "." << endl;
-            return -1;
         }
+        cout << "Erro: `config.erro` acusa erro " << config.error << "." << endl;
+        return -1;
     }
     if (config.help) {
         print_help();
         return 0;
     }
-    // carregando arquivo com puzzles e saindo caso nao consiga achar o arquivo
-    // de entrada ou nao consiga parsear os puzzles
     if (!file_exists(config.input_puzzle_file)) {
         cout << "Erro: nao eh possivel acessar o arquivo " << config.input_puzzle_file << endl;
         return -1;
     }
+    GameTables gametables = gen_gametables(config.input_puzzle_file);
+    if (gametables.tablecount == 0) {
+        cout << "Erro: `init_gametables` nao foi capaz de inicializar nenhuma gametable." << endl;
+        return -1;
+    }
+    print_gametable(gametables.tables[0]);
+    destroy_gametables(gametables);
     cout << "config gerada" << endl << config.error << endl << config.help << endl << config.ncheck << endl << config.input_puzzle_file << endl;
+    cout << gametables.tablecount << " tables gerados" << endl;
     return 0;
 }
 
