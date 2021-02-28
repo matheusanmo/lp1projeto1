da544dea7cfc7595912caa0789775c3e71ae67e9
matheus anmo <matheusanmo@gmail.com>
2021-02-28 19:57:25 -0300

    remove arquivos obsoletos, comeca a fazer CMakeLists.txt

--- src/cli.cpp
+++
@@ -1,9 +0,0 @@
-/* Trata corretamente os argumentos de linha de comando (5 pts);
-    ** Usage: sudoku [<options>] [<input_puzzle_file>]
-         Game options:
-           --ncheck    <num> Number of checks per game. Default = 3.
-           --help      Print this help text
-*/
-
-#include
-
